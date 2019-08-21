0cd1acf98c2ff95d564a2877b4fa4831145fc584
dionart <47128865+dionart@users.noreply.github.com>
2019-08-21 20:26:40 -0300

    Add files via upload

---
+++ IConta/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "IConta.h"
+#include "Conta.h"
+#include "ContaEspecial.h"
+#include "SaldoNaoDisponivelException.h"
+
+using namespace std;
+
+int main(void){
+    Conta *conta1 =  new Conta("DIOnizio", "00019232-2", 5000, 10000);
+
+    cout << "O nome do individuo eh: " << conta1->getNomeCliente() << endl;
+    cout << "O numero da conta do individuo eh: " << conta1->getNumeroConta() << endl;
+    cout << "O salario mensal do individuo eh: " << conta1->getSalarioMensal() << endl;
+    cout << "O saldo do individuo eh: " << conta1->getSaldo() << endl;
+    cout << "O limite do individuo eh: " << conta1->getLimite()<<endl;
+    conta1->sacar(2000);
+    cout << "O saldo do individuo eh: " << conta1->getSaldo() << endl;
+    conta1->depositar(1000);
+    cout << "O saldo do individuo eh: " << conta1->getSaldo() << endl;
+
+    cout<<endl;
+    ContaEspecial *conta_especial1 = new ContaEspecial("Rogerinho", "089101-1", 5000, 10000);
+
+    cout << "O nome do individuo eh: " << conta_especial1->getNomeCliente() << endl;
+    cout << "O numero da conta do individuo eh: " << conta_especial1->getNumeroConta() << endl;
+    cout << "O salario mensal do individuo eh: " << conta_especial1->getSalarioMensal() << endl;
+    cout << "O saldo do individuo eh: " << conta_especial1->getSaldo() << endl;
+    cout << "O limite do individuo eh: " << conta_especial1->getLimite()<<endl;
+    conta_especial1->sacar(2000);
+    cout << "O saldo do individuo eh: " << conta_especial1->getSaldo() << endl;
+    conta_especial1->depositar(1000);
+    cout << "O saldo do individuo eh: " << conta_especial1->getSaldo() << endl;
+    cout<<endl;
+
+    try{
+        cout<<"Teste exception 5000:"<<endl;
+        conta1->sacar(5000);
+    }catch(SaldoNaoDisponivelException s){
+        cerr<<"Error:"<<s.what()<<endl;
+    }catch(...){
+        cerr<<"Erro desconhecido"<<endl;
+    }
+
+    cout<<"Saldo apos retirada:"<<conta1->getSaldo()<<endl;
+    cout<<endl;
+
+    try{
+        cout<<"Teste exception 15000:"<<endl;
+        conta1->sacar(15000);
+    }catch(SaldoNaoDisponivelException s){
+        cerr<<"Error:"<<s.what()<<endl;
+    }catch(...){
+        cerr<<"Erro desconhecido"<<endl;
+    }
+
+    cout<<"Saldo apos retirada:"<<conta1->getSaldo()<<endl;
+    cout<<endl;
+
+
+    delete conta1;
+    delete conta_especial1;
+
+    return 0;
+}
