b362bb8dfa77dfcf568cdc43b6703d69913ddd79
dionart <47128865+dionart@users.noreply.github.com>
2019-08-22 01:04:45 -0300

    Add files via upload

---
+++ SistemaFuncionario/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "SistemaGerenciaFolha.h"
+
+using namespace std;
+
+int main()
+{
+    SistemaGerenciaFolha s1 =  SistemaGerenciaFolha();
+    Funcionario* f1 = new Assalariado("Arthur",1,1500);
+    Funcionario* f2 = new Horista("Samuel",2,300,3);
+    Funcionario* f3 = new Comissionado("Marcos",3,4,0.25,100);
+
+    cout<<"Salario:"<<f1->calculaSalario()<<endl;
+    cout<<"Salario:"<<f2->calculaSalario()<<endl;
+    cout<<"Salario:"<<f3->calculaSalario()<<endl;
+
+    s1.setFuncionarios(f1);
+    s1.setFuncionarios(f2);
+    s1.setFuncionarios(f3);
+
+    cout<<"Valor total da folha:"<<s1.calcularTotalFolha()<<endl;
+    cout<<"Salario do funcionario de matricula 1:"<<s1.consultaSalarioFuncionario(1)<<endl;
+    try{
+        cout<<"Buscando salario funcionario 4: "<<s1.consultaSalarioFuncionario(4)<<endl;
+    }catch(FuncionarioNaoExisteException e){
+        cerr<<"Error:"<<e.what()<<endl;
+    }
+
+    return 0;
+}
